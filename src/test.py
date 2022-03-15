a47b342a6655e2d568ebd0f345552fc2b4aaa33f
clegaard <clegaard@outlook.com>
2022-03-15 16:58:54 +0100

    ada

--- src/test.py
+++ src/test.py
@@ -2,6 +2,7 @@ import odrive
 from odrive.enums import AXIS_STATE_FULL_CALIBRATION_SEQUENCE, AXIS_STATE_CLOSED_LOOP_CONTROL, MOTOR_TYPE_HIGH_CURRENT, AXIS_STATE_IDLE, AXIS_STATE_ENCODER_INDEX_SEARCH, CONTROL_MODE_VELOCITY_CONTROL, CONTROL_MODE_POSITION_CONTROL, INPUT_MODE_VEL_RAMP
 import numpy as np
 import time
+import math
 from sys import exit
 import matplotlib.pyplot as plt
 od = odrive.find_any()
@@ -44,49 +45,78 @@ od.axis0.encoder.config.pre_calibrated = True
 # od.save_configuration()
 
 
+# od.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+# od.axis0.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
+# od.axis0.controller.config.vel_ramp_rate = 0.5
+# od.axis0.controller.config.input_mode = INPUT_MODE_VEL_RAMP
+# od.axis0.controller.input_vel = 2.0
+
+od.axis0.controller.config.vel_limit = 50.0
 od.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-od.axis0.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
-od.axis0.controller.config.vel_ramp_rate = 0.5
-od.axis0.controller.config.input_mode = INPUT_MODE_VEL_RAMP
-od.axis0.controller.input_vel = 2.0
+od.axis0.controller.config.control_mode = CONTROL_MODE_POSITION_CONTROL
+od.axis0.controller.config.circular_setpoints = False
+od.axis0.controller.input_pos = 0.0
 
 t_cur = time.time()
 t_start = t_cur
-t_last = time.time()
+t_sample_last = time.time()
+t_setpoint_last = time.time()
 ts = 1e-3
-t = []
+tsp = 1e-1
+timestamp_sample = []
+timestamp_setpoint = []
 q = []
+q_setpoint = []
 i = []
 t_duration = 5.0
 
 
+def pos_setpoint(t):
+    return 1.0
+    # return math.sin(t)*0.5 + 0.5
+
 while True:
     t_cur = time.time()
-    td = t_cur-t_last
-    if td >= ts:
-        t_last = t_cur
-        t.append(t_cur)
+
+    if t_cur-t_setpoint_last >= tsp:
+        t_setpoint_last = t_cur
+        qsp = pos_setpoint(t_cur - t_start)
+        timestamp_setpoint.append(t_cur)
+        od.axis0.controller.input_pos = qsp
+        q_setpoint.append(qsp)
+
+    if t_cur-t_sample_last >= ts:
+        t_sample_last = t_cur
+        timestamp_sample.append(t_cur)
         q.append(od.axis0.encoder.pos_estimate)
         i.append(od.axis0.motor.current_control.Iq_measured)
-
+        
     if t_cur - t_start > t_duration:
         break
 
-t = np.array(t)
+timestamp_sample = np.array(timestamp_sample)
+timestamp_setpoint = np.array(timestamp_setpoint)
 q = np.array(q)
+q_setpoint = np.array(q_setpoint)
 i = np.array(i)
-dt = t[1:]-t[:-1]
-t = t-t_start
+dt_sample = timestamp_sample[1:]-timestamp_sample[:-1]
+dt_sample = timestamp_setpoint[1:]-timestamp_setpoint[:-1]
+timestamp_sample = timestamp_sample-t_start
+dt_setpoint = timestamp_setpoint-t_start
 
 fig, (ax1,ax2) = plt.subplots(2,1,sharex=True)
-ax1.plot(t,q)
+ax1.plot(timestamp_sample,q, label="actual")
+ax1.plot(timestamp_setpoint,q_setpoint,label="setpoint")
 ax1.set_ylabel("q(t)")
-ax2.plot(t,i)
+ax2.plot(timestamp_sample,i)
 ax2.set_ylabel("i(t)")
 ax2.set_xlabel("t")
 
 fig, ax = plt.subplots()
-ax.hist(dt)
+ax.hist(dt_sample)
+
+fig, ax = plt.subplots()
+ax.hist(dt_setpoint)
 
 # timestamp, actual_q_0, actual_current_0
 
