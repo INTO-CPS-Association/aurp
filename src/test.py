9bb8a8d85c2f0322537392c915cd58c52155fd45
Emil Madsen <emil_madsen@hotmail.com>
2022-03-23 15:20:49 +0100

    corrected position control mode

--- src/test.py
+++ src/test.py
@@ -1,5 +1,5 @@
 import odrive
-from odrive.enums import AXIS_STATE_FULL_CALIBRATION_SEQUENCE, AXIS_STATE_CLOSED_LOOP_CONTROL, MOTOR_TYPE_HIGH_CURRENT, AXIS_STATE_IDLE, AXIS_STATE_ENCODER_INDEX_SEARCH, CONTROL_MODE_VELOCITY_CONTROL, CONTROL_MODE_POSITION_CONTROL, INPUT_MODE_VEL_RAMP
+from odrive.enums import AXIS_STATE_FULL_CALIBRATION_SEQUENCE, AXIS_STATE_CLOSED_LOOP_CONTROL, MOTOR_TYPE_HIGH_CURRENT, AXIS_STATE_IDLE, AXIS_STATE_ENCODER_INDEX_SEARCH, CONTROL_MODE_VELOCITY_CONTROL, CONTROL_MODE_POSITION_CONTROL, INPUT_MODE_VEL_RAMP, INPUT_MODE_POS_FILTER
 import numpy as np
 import time
 import math
@@ -9,14 +9,14 @@ od = odrive.find_any()
 
 # ================= Config =====================
 od.config.enable_brake_resistor = True
-od.config.brake_resistance = 2.2
-od.axis0.motor.config.torque_constant = 8.27 / 270
+od.config.brake_resistance = 2.2  # [A]
+od.axis0.motor.config.torque_constant = 8.27 / 270  # [Nm/A]
 od.axis0.motor.config.motor_type = MOTOR_TYPE_HIGH_CURRENT
-od.axis0.controller.config.vel_limit = 2.0
-od.axis0.motor.config.current_lim = 5.0
-od.axis0.motor.config.pole_pairs = 7
-od.axis0.encoder.config.cpr = 8192
-od.axis0.motor.config.calibration_current = 10.0
+od.axis0.controller.config.vel_limit = 2.0  # [turns/s]
+od.axis0.motor.config.current_lim = 10.0  # [A]
+od.axis0.motor.config.pole_pairs = 7  # [-]
+od.axis0.encoder.config.cpr = 8192  # [counts per revolution]
+od.axis0.motor.config.calibration_current = 13.0  # [A]
 
 # ================= Calibrate ===================
 od.axis0.encoder.config.use_index = True
@@ -42,27 +42,33 @@ if od.axis0.error != 0:
 
 od.axis0.encoder.config.pre_calibrated = True
 
-# od.save_configuration()
+od.save_configuration()
 
 
 # od.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 # od.axis0.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
 # od.axis0.controller.config.vel_ramp_rate = 0.5
 # od.axis0.controller.config.input_mode = INPUT_MODE_VEL_RAMP
-# od.axis0.controller.input_vel = 2.0
+# od.axis0.controller.input_vel = 1.0
 
 od.axis0.controller.config.vel_limit = 50.0
 od.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 od.axis0.controller.config.control_mode = CONTROL_MODE_POSITION_CONTROL
+od.axis0.controller.config.input_mode = INPUT_MODE_POS_FILTER
 od.axis0.controller.config.circular_setpoints = False
-od.axis0.controller.input_pos = 0.0
+od.axis0.controller.input_pos = 0.25
+od.axis0.controller.config.input_filter_bandwidth = 50.0  # [Hz]
+
+# while True:
+#     time.sleep(0.5)
+#     od.axis0.controller.input_pos = 0.25
 
 t_cur = time.time()
 t_start = t_cur
 t_sample_last = time.time()
 t_setpoint_last = time.time()
-ts = 1e-3
-tsp = 1e-1
+dt_sample = 1e-3
+tsp = 1e-2
 timestamp_sample = []
 timestamp_setpoint = []
 q = []
@@ -72,8 +78,8 @@ t_duration = 5.0
 
 
 def pos_setpoint(t):
-    return 1.0
-    # return math.sin(t)*0.5 + 0.5
+    # return 0.25
+    return math.sin(3*t)*0.5 + 0.5
 
 while True:
     t_cur = time.time()
@@ -85,7 +91,7 @@ while True:
         od.axis0.controller.input_pos = qsp
         q_setpoint.append(qsp)
 
-    if t_cur-t_sample_last >= ts:
+    if t_cur - t_sample_last >= dt_sample:
         t_sample_last = t_cur
         timestamp_sample.append(t_cur)
         q.append(od.axis0.encoder.pos_estimate)
@@ -99,26 +105,33 @@ timestamp_setpoint = np.array(timestamp_setpoint)
 q = np.array(q)
 q_setpoint = np.array(q_setpoint)
 i = np.array(i)
-dt_sample = timestamp_sample[1:]-timestamp_sample[:-1]
-dt_sample = timestamp_setpoint[1:]-timestamp_setpoint[:-1]
-timestamp_sample = timestamp_sample-t_start
-dt_setpoint = timestamp_setpoint-t_start
+dt_sample = timestamp_sample[1:] - timestamp_sample[:-1]
+dt_setpoint = timestamp_setpoint[1:] - timestamp_setpoint[:-1]
+timestamp_sample = timestamp_sample - t_start  # Zero timestamp
+timestamp_setpoint = timestamp_setpoint - t_start  # Zero timestamp
 
-fig, (ax1,ax2) = plt.subplots(2,1,sharex=True)
-ax1.plot(timestamp_sample,q, label="actual")
-ax1.plot(timestamp_setpoint,q_setpoint,label="setpoint")
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+ax1.plot(timestamp_sample, q, label="actual")
+ax1.plot(timestamp_setpoint, q_setpoint, label="setpoint")
 ax1.set_ylabel("q(t)")
-ax2.plot(timestamp_sample,i)
+ax2.plot(timestamp_sample, i)
 ax2.set_ylabel("i(t)")
 ax2.set_xlabel("t")
 
 fig, ax = plt.subplots()
 ax.hist(dt_sample)
+plt.xlabel('Time [s]')
+plt.ylabel('Amount [-]')
+plt.title('Sample Rate Histogram')
+
 
 fig, ax = plt.subplots()
 ax.hist(dt_setpoint)
+plt.xlabel('Time [s]')
+plt.ylabel('Amount [-]')
+plt.title('Setpoint Update Rate Histogram')
 
+# Generate CSV file with header:
 # timestamp, actual_q_0, actual_current_0
-
 
 plt.show()
